0b84dd39fa37bce1c5ac3cac82477bd03e3268ca
Neelchat <68162009+Neelchat@users.noreply.github.com>
2021-10-19 16:59:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-var SpeechRecognition = window.webkitSpeechRecognition
-var Recognition = new SpeechRecognition()
+var SpeechRecognition = window.webkitSpeechRecognition;
+ var recognition = new SpeechRecognition(); 
+function start() { document.getElementById("textbox").innerHTML = "";
+ recognition.start();
+ } recognition.onresult = function(event) { console.log(event);
+     var Content = event.results[0][0].transcript;
+      document.getElementById("textbox").innerHTML = Content;
+       console.log(Content);
 
-function Start(){
-    document.getElementById("textbox").innerHTML = " "
-    Recognition.start()
+ if(Content =="take my selfie") { console.log("taking selfie --- "); 
+speak() } } 
+function speak(){ var synth = window.speechSynthesis;
+     speak_data = "Taking your Selfie in 5 seconds";
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+       synth.speak(utterThis); Webcam.attach(camera);
+       setTimeout(function(){
+           take_snapshot()
+           save()
+           
+       },5000);
+   
+        
 
+       
+         } camera = document.getElementById("camera");
+          Webcam.set({
+               width:360, height:250, image_format : 'jpeg', jpeg_quality:90 
+            });
+
+function take_snapshot() {
+
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML = '<img id="selfie" src="'+data_url+'">'
+        
+    });
 }
-Recognition.onresult = function(event){
-    console.log(event)
-    content = event.results[0][0].transcript
-    document.getElementById("textbox").innerHTML = content
-    console.log(content)
+
+ function save(){
+link=document.getElementById("link");
+image = document.getElementById("selfie").src
+link.href = image
+link.click()
 }
